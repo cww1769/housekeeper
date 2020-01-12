5d3cab30873acbe865131ba0153707d581a65826
chen wei <cww1769@163.com>
2020-01-12 22:49:56 +0800

    add main activity

--- app/src/main/java/com/vivian/housekeeper/MainActivity.java
+++ app/src/main/java/com/vivian/housekeeper/MainActivity.java
@@ -1,22 +1,94 @@
 package com.vivian.housekeeper;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.flyco.tablayout.CommonTabLayout;
+import com.flyco.tablayout.listener.CustomTabEntity;
+import com.flyco.tablayout.listener.OnTabSelectListener;
+import com.flyco.tablayout.utils.UnreadMsgUtils;
+import com.flyco.tablayout.widget.MsgView;
+import com.vivian.housekeeper.util.TabEntity;
+import com.vivian.housekeeper.util.ViewFindUtils;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+    private Context mContext = this;
+    private ArrayList<Fragment> mFragments = new ArrayList<>();
+    private ArrayList<Fragment> mFragments2 = new ArrayList<>();
+
+    private String[] mTitles = {"我的小区", "智能家庭", "个人中心"};
+    private int[] mIconUnselectIds = {
+            R.mipmap.tab_home_unselect, R.mipmap.tab_contact_unselect, R.mipmap.tab_more_unselect};
+    private int[] mIconSelectIds = {
+            R.mipmap.tab_home_select,  R.mipmap.tab_contact_select, R.mipmap.tab_more_select};
+    private ArrayList<CustomTabEntity> mTabEntities = new ArrayList<>();
+    private View mDecorView;
+    private ViewPager mViewPager;
+    private CommonTabLayout mTabLayout_1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_common_tab);
 
-       if (!MyApplication.isLogged()) {
-            Intent intent = new Intent(this, LoginActivity.class);
-            startActivity(intent);
+        for (String title : mTitles) {
+            mFragments.add(SimpleCardFragment.getInstance("Switch ViewPager " + title));
         }
+
+
+        for (int i = 0; i < mTitles.length; i++) {
+            mTabEntities.add(new TabEntity(mTitles[i], mIconSelectIds[i], mIconUnselectIds[i]));
+        }
+
+        mDecorView = getWindow().getDecorView();
+        mViewPager = ViewFindUtils.find(mDecorView, R.id.vp_2);
+        mViewPager.setAdapter(new MyPagerAdapter(getSupportFragmentManager()));
+        /** with nothing */
+        mTabLayout_1 = ViewFindUtils.find(mDecorView, R.id.tl_1);
+
+        mTabLayout_1.setTabData(mTabEntities);
+        mTabLayout_1.showDot(2);
+    }
+
+    private class MyPagerAdapter extends FragmentPagerAdapter {
+        public MyPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return mTitles[position];
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+    }
+
+    protected int dp2px(float dp) {
+        final float scale = mContext.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
     }
 
 
